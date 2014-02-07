d93a292994f643db982da02fb29d985fbe4f882f
Cédric Van Frachem <cedric.vanfrachem@thalesgroup.com>
2014-02-06 17:35:11 -0800

    initial commit

---
+++ locksmgr/src/main/java/com/thalesgroup/services/dt/codingdojo/one/locksmgr/App.java
@@ -0,0 +1,13 @@
+package com.thalesgroup.services.dt.codingdojo.one.locksmgr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
