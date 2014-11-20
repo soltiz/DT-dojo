fe6d7c84cc18828dddc58c0a2a4b899d3d1a3999
Fabien Balageas <fabien.balageas@thalesgroup.com>
2014-11-20 13:10:39 +0100

    Added JarClassLoader (main and test)

---
+++ ComputeViewer/ComputeView/src/main/java/com/thales/services/dt/codingdojo/computerunner/JarClassLoader.java
@@ -0,0 +1,58 @@
+package com.thales.services.dt.codingdojo.computerunner;
+
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+
+public class JarClassLoader extends ClassLoader implements Closeable {
+
+	private static final int BUFFER_SIZE = 512;
+
+	private static final String CLASS_EXT = ".class";
+
+	private final JarFile m_file;
+
+	public JarClassLoader(File p_path) throws IOException {
+		if (p_path == null) {
+			throw new IllegalArgumentException("Path can't be null");
+		}
+
+		m_file = new JarFile(p_path);
+	}
+
+	@Override
+	protected Class<?> findClass(String p_name) throws ClassNotFoundException {
+		if (p_name == null) {
+			throw new IllegalArgumentException("Name can't be null");
+		}
+
+		ZipEntry entry = m_file.getEntry(p_name + CLASS_EXT);
+		if (entry == null) {
+			throw new ClassNotFoundException();
+		}
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+		try {
+			int read;
+			byte[] buffer = new byte[BUFFER_SIZE];
+			InputStream in = m_file.getInputStream(entry);
+
+			while ((read = in.read(buffer)) != -1) {
+				out.write(buffer, 0, read);
+			}
+		} catch (IOException ex) {
+			throw new ClassNotFoundException();
+		}
+
+		return defineClass(p_name, out.toByteArray(), 0, out.size());
+	}
+
+	public void close() throws IOException {
+		m_file.close();
+	}
+}
---
+++ ComputeViewer/ComputeView/src/test/java/com/thales/services/dt/codingdojo/computerunner/JarClassLoaderTest.java
@@ -0,0 +1,63 @@
+package com.thales.services.dt.codingdojo.computerunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JarClassLoaderTest {
+
+	private static final File RT_JAR_PATH = new File(System.getProperty("java.home") + "/lib/rt.jar");
+
+	@Test(expected = RuntimeException.class)
+	public void testNullJarName() throws IOException {
+		JarClassLoader cl = new JarClassLoader(null);
+	}
+
+	@Test(expected = IOException.class)
+	public void testJarNotFound() throws IOException {
+		JarClassLoader cl = new JarClassLoader(new File("notFound.jar"));
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testNullClassName() throws IOException, ClassNotFoundException {
+		JarClassLoader cl = null;
+		try {
+			cl = new JarClassLoader(RT_JAR_PATH);
+			Class<?> c = cl.loadClass(null);
+		} finally {
+			if (cl != null) {
+				cl.close();
+			}
+		}
+	}
+
+	@Test(expected = ClassNotFoundException.class)
+	public void testClassNotFound() throws IOException, ClassNotFoundException {
+		JarClassLoader cl = null;
+		try {
+			cl = new JarClassLoader(RT_JAR_PATH);
+			Class<?> c = cl.loadClass("foo.Bar");
+		} finally {
+			if (cl != null) {
+				cl.close();
+			}
+		}
+	}
+
+	@Test
+	public void testLoadClass() throws IOException, ClassNotFoundException {
+		JarClassLoader cl = null;
+		try {
+			cl = new JarClassLoader(RT_JAR_PATH);
+			Class<?> c = cl.loadClass("java.nio.ByteBuffer");
+			Assert.assertEquals(c, ByteBuffer.class);
+		} finally {
+			if (cl != null) {
+				cl.close();
+			}
+		}
+	}
+}
