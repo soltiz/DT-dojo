4bdc8ba3c85987f084d962429ffa001ee401db28
Cedric VAN FRACHEM <cedric.vanfrachem@thalesgroup.com>
2016-05-19 19:43:48 +0200

    added story 4 - last possible remaining value

--- sudoku/src/test/java/com/thalesgroup/services/cte/lis/dojo/Story3Test.java
+++ sudoku/src/test/java/com/thalesgroup/services/cte/lis/dojo/Story3Test.java
@@ -2,13 +2,13 @@ package com.thalesgroup.services.cte.lis.dojo;
 
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
-public class Story2Test extends SudokuTest{
+public class Story3Test extends SudokuTest{
 		
 		@Test
-		  public void testLastCellOfSquare() {
+		  public void testLastCellOfRow() {
 			
-			// When in a column, only on 1 cell is not solved, then this cell solution is the last digit
-			// not used in the row
+			// When in a square, only on 1 cell is not solved, then this cell solution is the last digit
+			// not used in the column
 			
 			SolvedProblem result = new SolvedProblem(new Integer[][]{
 					
@@ -16,17 +16,17 @@ public class Story2Test extends SudokuTest{
 					{  x,x,x,  x,x,x,  x,x,x  },
 					{  x,x,x,  x,x,2,  x,x,x  },
 					
-					{  x,x,x,  x,x,5,  x,x,x  },
-					{  x,x,x,  x,x,3,  x,x,x  },
-					{  x,x,x,  x,x,7,  x,x,x  },
+					{  x,x,x,  9,1,5,  x,x,x  },
+					{  x,x,x,  6,8,3,  x,x,x  },
+					{  x,x,x,  x,4,7,  x,x,x  },
 					
-					{  x,x,x,  x,x,1,  x,x,x  },
+					{  x,x,x,  x,x,x,  x,x,x  },
 					{  x,x,x,  x,x,9,  x,x,x  },
 					{  x,x,x,  x,x,6,  x,x,x  }
 					
 			});
 			
-			result.cell(2, 6).shallContain(8);
+			result.cell(6, 4).shallContain(2);
 			
 		  }
 
--- sudoku/src/test/java/com/thalesgroup/services/cte/lis/dojo/Story4Test.java
+++ sudoku/src/test/java/com/thalesgroup/services/cte/lis/dojo/Story4Test.java
@@ -2,13 +2,13 @@ package com.thalesgroup.services.cte.lis.dojo;
 
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
-public class Story2Test extends SudokuTest{
+public class Story4Test extends SudokuTest{
 		
 		@Test
-		  public void testLastCellOfSquare() {
+		  public void testLastPossibleValueOfCell() {
 			
-			// When in a column, only on 1 cell is not solved, then this cell solution is the last digit
-			// not used in the row
+			// When a cell belongs to a row, column and square, that already use all the digits but one,
+			// then the last possible value is set in the cell.
 			
 			SolvedProblem result = new SolvedProblem(new Integer[][]{
 					
@@ -16,17 +16,17 @@ public class Story2Test extends SudokuTest{
 					{  x,x,x,  x,x,x,  x,x,x  },
 					{  x,x,x,  x,x,2,  x,x,x  },
 					
-					{  x,x,x,  x,x,5,  x,x,x  },
-					{  x,x,x,  x,x,3,  x,x,x  },
-					{  x,x,x,  x,x,7,  x,x,x  },
+					{  7,x,x,  9,1,x,  x,8,x  },
+					{  x,x,x,  6,x,3,  x,x,x  },
+					{  x,x,x,  x,4,x,  x,x,x  },
 					
-					{  x,x,x,  x,x,1,  x,x,x  },
+					{  x,x,x,  x,x,x,  x,x,x  },
 					{  x,x,x,  x,x,9,  x,x,x  },
 					{  x,x,x,  x,x,6,  x,x,x  }
 					
 			});
 			
-			result.cell(2, 6).shallContain(8);
+			result.cell(4, 6).shallContain(5);
 			
 		  }
 
